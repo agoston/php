5b558e10a5899e42294a8278a368bd2b3b8b744c
Agoston Horvath <agoston@ripe.net>
2014-08-27 00:18:50 +0200

    simplified code & fixed mixed up temperature xpath filters

--- bbc.php
+++ bbc.php
@@ -1,11 +1,7 @@
 <?php
-class WeatherInfo {
-    public $day;
-    public $min;
-    public $max;
-}
 
-
+// TODO: [AH] add error handling
+// TODO: [AH] have an expert review this code
 class BBC
 {
     public function getWeather()
@@ -16,21 +12,24 @@ class BBC
         $dom->loadHTML($html);
         $xpath = new DOMXpath($dom);
 
-        $days = $xpath->query("//*[@id='weather_forecast']/dl/dt");
-        $maxtemps = $xpath->query("//*[@id='weather_forecast']/dl/dd[*]/p[@class='temp min']/span[@class='value']");
-        $mintemps = $xpath->query("//*[@id='weather_forecast']/dl/dd[*]/p[@class='temp max']/span[@class='value']");
+        $maxtemps = $xpath->query("//*[@id='weather_forecast']/dl/dd/p[@class='temp max']/span[@class='value']");
+        $mintemps = $xpath->query("//*[@id='weather_forecast']/dl/dd/p[@class='temp min']/span[@class='value']");
 
-        $result = array();
+        $result = [];
 
-        for ($i = 0; $i < $days->length; $i++) {
-            $weatherInfo = new WeatherInfo();
-            $weatherInfo->day = trim($days->item($i)->nodeValue);
-            $weatherInfo->min = trim($mintemps->item($i)->nodeValue);
-            $weatherInfo->max = trim($maxtemps->item($i)->nodeValue);
-            $result[] = $weatherInfo;
-        }
+        $this->addToResult($result, $maxtemps, "max");
+        $this->addToResult($result, $mintemps, "min");
 
         return $result;
+    }
+
+    private function addToResult(&$result, $temps, $key)
+    {
+        foreach ($temps as $temp) {
+            $dayNode = $temp->parentNode->parentNode->previousSibling->previousSibling;
+            $day = trim($dayNode->nodeValue);
+            $result[$day][$key] = trim($temp->nodeValue);
+        }
     }
 }
 
