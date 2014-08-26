7b7f2a0bc917974418bd3761f801a5349703cc50
Agoston Horvath <agoston@ripe.net>
2014-08-26 23:16:20 +0200

    return (still buggy) wather info

--- bbc.php
+++ bbc.php
@@ -1,8 +1,40 @@
 <?php
-$html = file_get_contents('http://www.bbc.com/');
-$dom = new DOMDocument();
-$dom->loadHTML($html);
-$div = $dom->getElementById('weather');
+class WeatherInfo {
+    public $day;
+    public $min;
+    public $max;
+}
 
-$content = $dom->saveHTML($div);
-print($content);
+
+class BBC
+{
+    public function getWeather()
+    {
+        $html = file_get_contents('http://www.bbc.com/');
+
+        $dom = new DOMDocument();
+        $dom->loadHTML($html);
+        $xpath = new DOMXpath($dom);
+
+        $days = $xpath->query("//*[@id='weather_forecast']/dl/dt");
+        $maxtemps = $xpath->query("//*[@id='weather_forecast']/dl/dd[*]/p[@class='temp min']/span[@class='value']");
+        $mintemps = $xpath->query("//*[@id='weather_forecast']/dl/dd[*]/p[@class='temp max']/span[@class='value']");
+
+        $result = array();
+
+        for ($i = 0; $i < $days->length; $i++) {
+            $weatherInfo = new WeatherInfo();
+            $weatherInfo->day = trim($days->item($i)->nodeValue);
+            $weatherInfo->min = trim($mintemps->item($i)->nodeValue);
+            $weatherInfo->max = trim($maxtemps->item($i)->nodeValue);
+            $result[] = $weatherInfo;
+        }
+
+        return $result;
+    }
+}
+
+$bbcClient = new BBC();
+$weather = $bbcClient->getWeather();
+
+var_dump($weather);
